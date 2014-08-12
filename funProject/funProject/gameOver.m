c35e27f00cee0a1a547db7d804ed90fb7a40db4b
iampgzp <jp3242@columbia.edu>
2014-08-12 17:13:05 -0400

    Add two buttons in game over scene

--- funProject/funProject/gameOver.m
+++ funProject/funProject/gameOver.m
@@ -7,6 +7,7 @@
 //
 
 #import "gameOver.h"
+#import "introScene.h"
 
 
 @implementation gameOver
@@ -25,16 +26,44 @@
     if (!self) return(nil);
     
     // Hello world
-    CCLabelTTF *label = [CCLabelTTF labelWithString:[NSString stringWithFormat:@"Game Over %ld", (long)score] fontName:@"Chalkduster" fontSize:36.0f];
+    CCLabelTTF *label = [CCLabelTTF labelWithString:[NSString stringWithFormat:@"Game Over"] fontName:@"Chalkduster" fontSize:36.0f];
     label.positionType = CCPositionTypeNormalized;
     label.color = [CCColor redColor];
     label.position = ccp(0.5f, 0.5f); // Middle of screen
     [self addChild:label];
-
+    
+    CCLabelTTF *scoreLabel = [CCLabelTTF labelWithString:[NSString stringWithFormat:@"%ld", (long)score] fontName:@"Chalkduster" fontSize:36.0f];
+    scoreLabel.positionType = CCPositionTypeNormalized;
+    scoreLabel.color = [CCColor redColor];
+    scoreLabel.position = ccp(0.5f, 0.40f); // Middle of screen
+    [self addChild:scoreLabel];
+    
+    
+    CCButton *retryButton = [CCButton buttonWithTitle:@"[ Retry ]" fontName:@"Verdana-Bold" fontSize:18.0f];
+    retryButton.positionType = CCPositionTypeNormalized;
+    retryButton.position = ccp(0.25f, 0.25f);
+    [retryButton setTarget:self selector:@selector(onRetryClicked:)];
+    [self addChild:retryButton];
+    
+    
+    CCButton *shareButton = [CCButton buttonWithTitle:@"[ Share ]" fontName:@"Verdana-Bold" fontSize:18.0f];
+    shareButton.positionType = CCPositionTypeNormalized;
+    shareButton.position = ccp(0.75f, 0.25f);
+    [shareButton setTarget:self selector:@selector(onShareClicked:)];
+    [self addChild:shareButton];
     
     // done
 	return self;
 }
+
+- (void)onRetryClicked:(CCButton *) sender{
+    [[CCDirector sharedDirector] replaceScene:[IntroScene scene] withTransition:[CCTransition transitionMoveInWithDirection:CCTransitionDirectionDown duration:1.0f]];
+}
+
+- (void)onShareClicked:(CCButton *) sender{
+    
+}
+
 
 
 @end
